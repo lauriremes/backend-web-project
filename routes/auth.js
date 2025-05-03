ed8dd18f45633c213e8a20ab623c27c91f3cf336
MiikaKonttila <miika20113@student.hamk.fi>
2025-05-03 13:10:16 +0300

    installed bcrypt and created password hashing and login

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
-// testing registration, does not hash the password
+// testing registration
 router.post('/register', async (req, res) => {
     const { name, email, password } = req.body;
   
@@ -22,13 +23,16 @@ router.post('/register', async (req, res) => {
       user = new User({
         name,
         email,
-        password //plain text password need to hash this
+        password
       });
+
+      const salt = await bcrypt.genSalt(10)
+      user.password = await bcrypt.hash(password, salt);
   
       // save the user to the database
       await user.save();
   
-      // send back success response avoid sending password back
+      // send back success response
       res.status(201).json({
         _id: user._id,
         name: user.name,
@@ -47,5 +51,30 @@ router.post('/register', async (req, res) => {
       res.status(500).json({ message: 'server error' });
     }
   });
-  
-  module.exports = router;
+
+router.post('/login', async (req, res) => {
+  const {email, password} = req.body;
+  if (!email || !password){
+    return res.status(400).json({message: 'provide email and password'});
+  }
+  try{
+    const user = await User.findOne({email});
+
+    if(!user){
+      return res.status(400).json({message: 'wrong credentials'});
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch){
+      return res.status(400).json({message: 'wrong credentials'});
+    }
+
+    res.json({message: 'login succesful'});
+
+  } catch (err){
+    console.error(err.messsage);
+    res.status(500).json({message: 'server error'});
+  }
+});
+module.exports = router;
